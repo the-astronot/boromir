f8efdffbd39a290245464cf6ea7542102705baf4
the-astronot <maxtmarshall99@gmail.com>
2025-01-17 02:38:11 -0500

    - Added file permission warning to setup file

--- setup.sh
+++ setup.sh
@@ -6,16 +6,22 @@
 ################################################################################
 
 # Download NAIF Spice Kernels
-setup/kernel_downloader.sh
+echo "Begin Downloading Spice Kernels"
+setup/kernel_downloader.sh || echo "Bad permissions" && exit
+echo "End Downloading Spice Kernels"
 
 # Download Required DEM/Albedo Files
-setup/dem_downloader.sh
+echo "Begin Downloading DEM/Albedo Files"
+setup/dem_downloader.sh || echo "Bad permissions" && exit
+echo "End Downloading DEM/Albedo Files"
 
 # Prep Blender installs
+echo "Install Blender Python Libraries"
 setup/setup_blender.sh
 
 # Build C++ Files
-src/cpp/buildmesh.sh
+echo "Build C++ Script"
+src/cpp/buildmesh.sh || echo "Bad permissions" && exit
 
 # Create Virtual Environment
 if [[ ! -e ".venv" ]]; then
@@ -27,6 +33,7 @@ else
 fi
 
 # Source the Venv
+echo "Upgrading Pip"
 source ".venv/bin/activate"
 pip3 install --upgrade pip
 
