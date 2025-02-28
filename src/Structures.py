- Fixed quaternion orientation - Got pose combiner working - Think I fixed the setup issues

src/Structures.py
@@ -80,12 +80,23 @@ class Quaternion():
 	def toArray(self):
 		return array([self.s,self.v[0],self.v[1],self.v[2]])
 	
+	def fromArray(self,arr):
+		"""
+			Assumes that array is set up like: [s,v1,v2,v3]
+		"""
+		self.s = float(arr[0])
+		self.v = np.array(arr[1:4],dtype=float)
 
-# Common Quaternions
-#quatWorldtoCam = Quaternion()
-#quatWorldtoCam.fromDCM(array([[0,0,1],
-#															[-1,0,0],
-#															[0,-1,0]]))
+
+def quat_mult(q1,q2):
+	"""
+		Multiply q1 by q2
+	"""
+	q3 = Quaternion()
+	q3.s = q1.s*q2.s-(np.sum(q1.v*q2.v))
+	q3.v = q1.s*q2.v + q2.s*q1.v + np.cross(q1.v,q2.v)
+	return q3
+
 
 
 class State():
@@ -97,6 +108,12 @@ class State():
 
 	def __init__(self,position=None,attitude=None):
 		if position is not None:
-			self.position = array(position)
+			self.position = array(position,dtype=float)
 		if attitude is not None:
 			self.attitude = attitude
+
+	def copy(self):
+		new_state = State()
+		new_state.position = self.position
+		new_state.attitude = Quaternion(self.attitude.s,self.attitude.v)
+		return new_state

src/file_io.py
@@ -3,7 +3,6 @@
 import json
 import os
 from os.path import exists,join,abspath,dirname,basename,isdir,isfile
-import contextlib
 import numpy as np
 
 # Local imports
@@ -81,7 +80,7 @@ def read_traj_file(filename):
 		data = entry.strip(",").split(",")
 		name = data[0]
 		cam_pos = np.array(data[1:4],dtype=float)
-		cam_quat = Quaternion(data[4],np.array(data[5:8],dtype=float))
+		cam_quat = Quaternion(data[4],-np.array(data[5:8],dtype=float))
 		cam_state = State(position=cam_pos,attitude=cam_quat)
 		time = None
 		sun_los = None

src/gkmethod.py
@@ -27,15 +27,7 @@ def gkmethod(camera):
 	# Let's Build a Lower-Poly Moon
 	ra_sections = 360
 	decl_sections = 180
-
-	# Create arrays of ra and decl values
-	ra = np.linspace(0,2*np.pi,ra_sections,endpoint=False)
-	ras = np.tile(ra,decl_sections).reshape(decl_sections,ra_sections)
-	decl = np.linspace(-np.pi/2,np.pi/2,decl_sections)
-	decls = np.repeat(decl,ra_sections).reshape(decl_sections,ra_sections)
-
-	# Convert ra and decls to xyz coords
-	xyzs = array([RADIUS*cos(decls)*cos(ras),RADIUS*cos(decls)*sin(ras),RADIUS*sin(decls)]).transpose((2,1,0))
+	xyzs = generate_moon_map(ra_sections=ra_sections,decl_sections=decl_sections)
 
 	# Find camera FOV bounds
 	fov_bounds = cos(sqrt(camera.FOV_x**2+camera.FOV_y**2)/2)
@@ -50,7 +42,7 @@ def gkmethod(camera):
 	
 	# Calculate offnadir angle
 	unity_pos = (pos/norm(pos)).reshape(3,)
-	offnadir = arccos(dot(unity_pos,-los))
+	offnadir = arccos(min(1,max(dot(unity_pos,-los),-1)))
 	debug("NADIR Angle = {}".format(rad2deg(offnadir)))
 	if offnadir < OFFNADIR_THRESH:
 		info("NADIR Angle within tolerance, aborted GKM")
@@ -64,42 +56,97 @@ def gkmethod(camera):
 	nabla = dot(-moon_los,pos)**2-(norm(pos)**2-RADIUS**2)
 	min_dist = abs(-dot(-moon_los,pos)+sqrt(nabla))
 	angles = np.where((min_dist+TOL).reshape(360,180)>=norm(diff,axis=2),np.dot(moon_los,los),-1)
-	blob_target = np.where(angles>fov_bounds,norm(diff,axis=2),-1)
+	blob_target = np.where(angles>fov_bounds,norm(diff,axis=2),0)
 
-	# Finding the center of the blob
-	blob_xyzs = np.where(blob_target.reshape(360,180,1)>-1,xyzs,array([0,0,0]))
-	blob_mask = np.array(np.where(norm(blob_xyzs,axis=2)>0))
+	# Finding the locations on the blob
+	blob_xyzs = np.where(blob_target.reshape(360,180,1)>0,xyzs,array([0,0,0]))
+	
+	# Get camera pose from coverage blob
+	camera = get_camera_from_coverage(blob_xyzs,camera)
 
-	pts_xyzs = blob_xyzs[blob_mask[0,:],blob_mask[1,:],:]
+	return camera
+
+
+def get_coverage(pos,los,fov,ra=360):
+	"""
+		Determine how much of the moon can be seen with the current camera state
+	"""
+	moon_xyzs = generate_moon_map(ra_sections=ra,decl_sections=ra//2)
+	shape = array(moon_xyzs.shape)
+	#info("Shape is: {}".format(shape))
+	shape[2] = 1
+	diff = moon_xyzs-pos
+	fov_bounds = cos(fov/2)
+	max_dist = sqrt(norm(pos)**2+RADIUS**2)
+	moon_los = diff/norm(diff,axis=2).reshape(shape)
+	angles = np.where(norm(diff,axis=2)<=max_dist,np.dot(moon_los,los),0)
+	blob = np.where(angles>fov_bounds,norm(diff,axis=2),0)
+	return blob
+
+
+def get_coverage_from_state(state,fov,ra=360):
+	"""
+		Determine how much of the Moon can be seen with the current camera pose
+	"""
+	position = state.position
+	los = state.attitude.toDCM()@np.array([0,0,1])
+	#print("Pos,Los = {},{}".format(position,los))
+	return get_coverage(position,los,fov,ra)
+
+
+def get_camera_from_coverage(coverage,camera):
+	"""
+		Generate a NADIR-pointing state for a moon map
+	"""
+	ra_sec,decl_sec = coverage.shape
+	#info("Coverage shape: {},{}".format(ra,decl))
+	# Generate Moon coords again
+	xyzs = generate_moon_map(ra_sections=ra_sec,decl_sections=decl_sec)
+
+	# Finding the center of the coverage
+	cov_xyzs = np.where(coverage.reshape(360,180,1)>0,xyzs,array([0,0,0]))
+	cov_mask = np.array(np.where(norm(cov_xyzs,axis=2)>0))
+
+	pts_xyzs = cov_xyzs[cov_mask[0,:],cov_mask[1,:],:]
 	if len(pts_xyzs) == 0:
 		# Typically not good, no Moon found in sight
 		warning("No Moon in View")
-		return camera
-	
+		return None
+
 	ctr_xyz = np.average(pts_xyzs,axis=0)
 	ctr_los = ctr_xyz/norm(ctr_xyz)
+	#info("CTR_XYZ: {}".format(ctr_xyz))
 	decl = arcsin(ctr_los[2])
 	if decl > np.pi/2:
 		decl -= np.pi
+		ra -= np.pi
 	ra = arccos(ctr_los[0]/cos(decl))
-	debug("GKM CAMERA LOCATION = {} N, {} E".format(rad2deg(ra),rad2deg(decl)))
+	#debug("GKM CAMERA LOCATION = {} N, {} E".format(rad2deg(decl),rad2deg(ra)))
 
 	# Finding the furthest extremities
-	pos2 = ctr_los*RADIUS*1.2
-	los2 = -ctr_los
+	pos = ctr_los*RADIUS*1.2
+	los = -ctr_los
 	blob_prev = 0
-	blob = get_coverage(pos2,los2,xyzs,min([camera.FOV_x,camera.FOV_y]))
-	cont = len(np.where(blob_target>blob)[0])
-	while cont > 0 and np.sum(blob) != np.sum(blob_prev):
+	blob = get_coverage(pos,los,min([camera.FOV_x,camera.FOV_y]),ra=ra_sec)
+	#info("Blob shape: {}".format(blob.shape))
+	cont = len(np.where(coverage>blob)[0])
+	count = 0
+	while cont > 0 and count < 5:
 		blob_prev = blob
-		pos2*=1.2
-		blob = get_coverage(pos2,los2,xyzs,min([camera.FOV_x,camera.FOV_y]))
-		cont = len(np.where(blob_target>blob)[0])
+		pos*=1.2
+		blob = get_coverage(pos,los,min([camera.FOV_x,camera.FOV_y]),ra=ra_sec)
+		#info("Blob shape: {}".format(blob.shape))
+		cont = len(np.where(coverage>blob)[0])
+		if np.sum(blob) != np.sum(blob_prev):
+			count += 1
+		else:
+			count = 0
 	if np.sum(blob) == np.sum(blob_prev):
 		warning("GK Method reached Maximum View without covering Target Area")
-
+	
+	debug("GKM CAMERA LOCATION = {}".format(pos))
 	# Build the GKM camera state
-	global_z = los2
+	global_z = los
 	global_x = array([-sin(ra),cos(ra),0])
 	global_y = mu.rot_about(global_z,global_x,np.pi/2)
 	local_z = array([0,0,1])
@@ -110,22 +157,36 @@ def gkmethod(camera):
 	
 	# Convert to Quaternion and assign to camera
 	quat = Quaternion()
-	quat.fromDCM(dcm)
-	camera.set_state(State(pos2,quat))
+	quat.fromDCM(dcm.T)
+	debug("GKM CAMERA QUAT = {}".format(quat))
+	camera.set_state(State(pos,quat))
 
 	return camera
 
 
-def get_coverage(pos,los,moon_xyzs,fov):
-	"""
-		Determine how much of the moon can be seen with the current camera state
-	"""
-	shape = array(moon_xyzs.shape)
-	shape[2] = 1
-	diff = moon_xyzs-pos
-	fov_bounds = cos(fov/2)
-	max_dist = sqrt(norm(pos)**2+RADIUS**2)
-	moon_los = diff/norm(diff,axis=2).reshape(shape)
-	angles = np.where(norm(diff,axis=2)<=max_dist,np.dot(moon_los,los),-1)
-	blob = np.where(angles>fov_bounds,norm(diff,axis=2),-1)
-	return blob
+def generate_moon_map(ra_sections=360,decl_sections=180):
+	# Let's build a Moon model
+	ra_sections = int(ra_sections)
+	decl_sections = int(decl_sections)
+
+	#info("RA,Decl = {},{}".format(ra_sections,decl_sections))
+
+	# Create arrays of ra and decl values
+	ra = np.linspace(0,2*np.pi,ra_sections,endpoint=False)
+	ras = np.tile(ra,decl_sections).reshape(decl_sections,ra_sections)
+	decl = np.linspace(-np.pi/2,np.pi/2,decl_sections)
+	decls = np.repeat(decl,ra_sections).reshape(decl_sections,ra_sections)
+
+	# Convert ra and decls to xyz coords
+	xyzs = array([RADIUS*cos(decls)*cos(ras),RADIUS*cos(decls)*sin(ras),RADIUS*sin(decls)]).transpose((2,1,0))
+	return xyzs
+
+
+if __name__ == "__main__":
+	# Quick checks
+	quat = Quaternion()
+	quat.fromArray(np.array([0.50000,0.50000,0.50000,-0.50000]))
+	state = State(position=np.array([3237400,0,0]),attitude=quat)
+	moon_map = generate_moon_map(ra_sections=20,decl_sections=10)
+	fov = np.deg2rad(20)
+	get_coverage_from_state(state,fov)

src/pose_combiner.py
@@ -1,9 +1,55 @@
 # Library imports
-
+import numpy as np
 
 # Local imports
+import gkmethod as gk
+from Log import debug,info
 
 
 # TODO: Write a function to combine similar poses
-def combine_poses():
-	return
+def combine_poses(poses,camera,threshold=.7,ra=360):
+	if len(poses) == 0:
+		return []
+	comb_poses = []
+	comb_maps = []
+	cameras = []
+	#info("Base Camera Position: {}".format(camera.state.position))
+	for i,pose in enumerate(poses):
+		added = False
+		fov_max = np.sqrt(camera.FOV_y**2+camera.FOV_x**2)
+		coverage = gk.get_coverage_from_state(pose.cam_state,fov_max,ra)
+		coverage = np.where(coverage>0,1,0)
+		for j,exist_pose in enumerate(comb_poses):
+			# Compare: if match, add pose and continue
+			if compare_poses(coverage,comb_maps[j],threshold=threshold):
+				debug("Combined 2 Maps!")
+				comb_poses[j].append(pose)
+				comb_maps[j] = comb_maps[j]+coverage
+				added = True
+				break
+		if not added:
+			debug("Added new map!")
+			comb_poses.append([pose])
+			comb_maps.append(coverage)
+	for map in comb_maps:
+		cameras.append(gk.get_camera_from_coverage(map,camera.copy()))
+		#info("Mesh Camera Position: {}".format(cameras[-1].state.position))
+	return comb_poses, cameras
+
+def compare_poses(map1,map2,threshold):
+	"""
+		Checks whether visible maps for separate poses should be combined
+	"""
+	total1 = np.sum(map1)
+	total2 = np.sum(map2)
+	if total1 == 0 or total2 == 0:
+		if total1 != total2:
+			return False
+		return True
+	mask1 = np.where(map1>0,1,0)
+	mask2 = np.where(map2>0,1,0)
+	union1 = np.sum(map1*mask2)
+	union2 = np.sum(map2*mask1)
+	if (union1/total1)>threshold and (union2/total2)>threshold:
+		return True 
+	return None